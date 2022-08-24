6d32ab741dd5ffa40dae88c257486bc1ba252733
Lucas Barbosa <lukeb.trybe@gmail.com>
2022-08-23 23:45:07 -0300

    feat: add helpers funcs

---
+++ cmd/api/helpers.go
@@ -0,0 +1,63 @@
+package main
+
+import (
+	"encoding/json"
+	"errors"
+	"io"
+	"net/http"
+)
+
+func (app *application) readJson(w http.ResponseWriter, r *http.Response, data interface{}) error {
+	maxBytes := 1048576 // 1MB
+	r.Body = http.MaxBytesReader(w, r.Body, int64(maxBytes))
+
+	dec := json.NewDecoder(r.Body)
+	err := dec.Decode(data)
+	if err != nil {
+		return err
+	}
+
+	err = dec.Decode(&struct{}{})
+	if err != io.EOF {
+		return errors.New("body must contain one json value only")
+	}
+
+	return nil
+}
+
+func (app *application) writeJson(w http.ResponseWriter, statusCode int, data interface{}, headers ...http.Header) error {
+
+	out, err := json.MarshalIndent(data, "", "\t")
+	if err != nil {
+		return err
+	}
+
+	if len(headers) > 0 {
+		for i, v := range headers[0] {
+			w.Header()[i] = v
+		}
+	}
+
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(statusCode)
+	_, err = w.Write(out)
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
+
+func (app *application) errorJson(w http.ResponseWriter, err error, status ...int) {
+	statusCode := http.StatusBadRequest
+
+	if len(status) > 0 {
+		statusCode = status[0]
+	}
+
+	var payload jsonResponse
+	payload.Error = true
+	payload.Message = err.Error()
+
+	app.writeJson(w, statusCode, payload)
+}
