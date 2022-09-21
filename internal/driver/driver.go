b0596faeb894f5afae2e0922a49a03912b8cdc9b
Lucas Barbosa <lukeb.trybe@gmail.com>
2022-09-21 00:49:40 -0300

    feat: add driver package

---
+++ internal/driver/driver.go
@@ -0,0 +1,47 @@
+package driver
+
+import (
+	"database/sql"
+	"fmt"
+	"time"
+
+	_ "github.com/jackc/pgconn"
+	_ "github.com/jackc/pgx/v4"
+	_ "github.com/jackc/pgx/v4/stdlib"
+)
+
+type DB struct {
+	SQL *sql.DB
+}
+
+var dbConn = new(DB)
+
+const maxOpenDbConn = 5
+const maxIdleDbConn = 5
+const maxDbLifeTime = 5 * time.Minute
+
+func ConnectPostgres(dsn string) (*DB, error) {
+	db, err := sql.Open("pgx", dsn)
+	if err != nil {
+		return nil, err
+	}
+
+	db.SetMaxOpenConns(maxOpenDbConn)
+	db.SetMaxIdleConns(maxIdleDbConn)
+	db.SetConnMaxLifetime(maxDbLifeTime)
+
+	err = testDb(err, db)
+
+	dbConn.SQL = db
+
+	return dbConn, err
+}
+
+func testDb(err error, db *sql.DB) error {
+	err = db.Ping()
+	if err != nil {
+		fmt.Println("Error:", err)
+	}
+
+	return err
+}
